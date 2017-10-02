c595108d5e75c9ccc52f1640f4fceed77ff6c78f
John S. Dixon <johnsdixon@gmail.com>
2017-10-02 15:17:45 +0100

    Add call to ip-api.com for IP address details

--- IPDetails.py
+++ IPDetails.py
@@ -2,12 +2,19 @@
 # -*- coding: utf-8 -*-
 
 import time
+import httplib
 
 with open ("IPaddrs.txt","r") as openfileobject:
 	for line in openfileobject:
 		ipAddr = line.strip()
-		print ipAddr
-		time.sleep(.5)
+		if ipAddr:
+			time.sleep(.5)
+
+			http = httplib.HTTPConnection('ip-api.com')
+			http.request("GET","/csv/"+ipAddr)
+			resp = http.getresponse()
+			print resp.read()
+			http.close()
 
 openfileobject.close()
 
