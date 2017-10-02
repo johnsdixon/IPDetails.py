bdd51349e4d0b55cc3c50f75e50dcb32853dca48
John S. Dixon <johnsdixon@gmail.com>
2017-10-02 17:22:56 +0100

    First pass parsing for output.. it's messy so look at other options

--- IPDetails.py
+++ IPDetails.py
@@ -4,18 +4,33 @@
 import time
 import httplib
 
-with open ("IPaddrs-short.txt","r") as openfileobject:
+# Print the CSV output headers - might use csv library to do this neater
+print "Id,Label,AS#,AS Name,Country,Region"
+
+# Get a connection to the webserver
+http = httplib.HTTPConnection('ip-api.com')
+
+with open ("IPaddrs.txt","r") as openfileobject:
 	for line in openfileobject:
 		ipAddr = line.strip()
 		if ipAddr:
+
+# Don't overload the API server,
+# wait half a second between iterations
 			time.sleep(.5)
 
-			http = httplib.HTTPConnection('ip-api.com')
 			http.request("GET","/csv/"+ipAddr)
 			resp = http.getresponse()
-			print resp.read()
-			http.close()
+			data = resp.read().split(",")
 
+			if data[0] == 'fail':
+				print '"',data[2],'","',data[2],'",,',data[1],',"XX","XXX"'
+			elif data[0] == 'success':
+				print "Success"
+			else:
+				print "Broken"
+		
+http.close()
 openfileobject.close()
 
 
