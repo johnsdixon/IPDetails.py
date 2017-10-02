47d2caf5895ec44c5b35abe1feb231f8f38412b4
John S. Dixon <johnsdixon@gmail.com>
2017-10-02 15:03:36 +0100

    Add time delay to avoid oversubscribing the remote API

--- IPDetails.py
+++ IPDetails.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import time
+
 with open ("IPaddrs.txt","r") as openfileobject:
 	for line in openfileobject:
 		ipAddr = line.strip()
 		print ipAddr
-
+		time.sleep(.5)
 
 openfileobject.close()
 
