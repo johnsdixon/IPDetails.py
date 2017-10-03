7d145fd08db880acce12f21e471a6b7183d32799
John S. Dixon <johnsdixon@gmail.com>
2017-10-03 10:37:15 +0100

    Mostly done. Need to tidy split of ASN and ASName

--- IPDetails.py
+++ IPDetails.py
@@ -20,22 +20,33 @@ with open ("IPaddrs-short.txt","r") as inputhandle:
 		ipAddr = line.strip()
 		if ipAddr:
 
-# Don't overload the API server,
-# wait half a second between iterations
-			time.sleep(.5)
-
 			http.request("GET","/csv/"+ipAddr)
 			resp = http.getresponse()
-			data = resp.read().split(",")
+			# Don't overload the API server,
+			# wait half a second between iterations
+			data = resp.read().split(',')
+			time.sleep(.5)
 			
 			if data[0] == 'success':
-				print ipAddr," responded with IP Details for AS", data[12]
-# need to split returned AS field on " "
-				outputhandle.writerow({'Id':data[13],'Label':data[13],'AS#':data[12],'AS Name':data[12],'Country Code':data[2],'Country':data[1],'Region Code':data[3],'Region':data[4]})
+				print ipAddr," responded with IP Details for ", data[12]
+
+				asn,space,asname = data[12].partition(' ')
+				# Now we need to strip first extra quote and AS from ASN
+				# and last extra quote from asname.
+				asn = asn[3:]
+				asname = asname[:-1]
+				outputhandle.writerow({'Id':data[13],'Label':data[13],'AS#':asn,'AS Name':asname,'Country Code':data[2],'Country':data[1],'Region Code':data[3],'Region':data[4]})
 			else:
-				print data
-				print ipAddr," responded as a reserved address"
-				outputhandle.writerow({'Id':data[2],'Label':data[2],'AS#':'','AS Name':'','Country Code':'XX','Country':'','Region Code':'XXX','Region':''})
+				if data[1]=='"invalid query"':
+					print ipAddr," is malformated, produced Invalid Query"
+				elif data[1]=='"quota"':
+					print "You are over quota - break your input file, or add a larger delay"
+					print ipAddr," should be the start of your next file"
+				else:
+					asname = data[1]
+					asname = asname[1:-1]
+					print ipAddr," responded as a reserved address"
+					outputhandle.writerow({'Id':data[2],'Label':data[2],'AS#':'','AS Name':asname,'Country Code':'XX','Country':'','Region Code':'XXX','Region':''})
 		
 http.close()
 inputhandle.close()
