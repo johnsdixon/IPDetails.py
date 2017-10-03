12954f42ce803a0c55e3bb428d6e92abe3f797e1
John S. Dixon <johnsdixon@gmail.com>
2017-10-03 19:44:18 +0100

    Add CSV output.. not sure about the eval, as we download JSON data, perhaps JSON.load would be better

--- IPDetails.py
+++ IPDetails.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import json
-import io
 import csv
-import time
 import httplib
+import time
 
-inputfilename = 'IPAddrs-short.txt'
+inputfilename = 'IPAddrs.txt'
 outputfilename = 'IPAddrs.out'
 csvdialect = {'__module__': 'csv', 'lineterminator': '\r\n', 'skipinitialspace': False, 'quoting': 0, '_name': 'sniffed', 'delimiter': ',', 'quotechar': '"', '__doc__': None, 'doublequote': False}
 
-outfields = ['Id','Label','AS#','AS Name','Country Code','Country','Region Code','Region']
+outfields = ['Id','Label','AS#','ASName','as','isp','org','status','countryCode','country','region','regionName','city','zip','lat','lon','timezone','message','query']
 apifields = ['success','Country','Country Code','Region Code','Region','City','Zip','Lat','Long','TZ','ISP Name','Org Name','ASN Name','QueryIP']
 
 datablock = {}
@@ -27,8 +25,6 @@ inputhandle.close()
 # We now have the list of IP addresses we want to look at in the datablock,
 # with an empty dict as placeholders for values
 
-#apihandle = io.StringIO()
-
 # Get a connection to the webserver
 http = httplib.HTTPConnection('ip-api.com')
 
@@ -41,64 +37,28 @@ for ipAddr in datablock:
 			http.request("GET","/json/"+ipAddr)
 			resp = http.getresponse()
 
-			# This is causing issues - need to refactor for CSV 
-			# (entries with , break things
-			# e.g. "AS6939 Hurricane Electric, Inc.")
-			# data = resp.read().split(',')
-
-			print ipAddr, datablock[ipAddr]
-
 			data=resp.read()
-			print data
 			l={ipAddr:data}
-			print l
-			datablock.update(dict(l))
+			datablock.update(l)
 
-			print ipAddr, datablock[ipAddr]
+# In this block we need to look at spliting out the AS# and ASName from
+# the as field from the API.
+# Also need to do a reverse lookup on the IP Address (it might be useful)
+# (and it burns time for the API, which we can subtract from the delay.)
 
-#			# The new way
-#			reader = csv.reader(resp.read())
-#			print reader
-#			raw_input("Press ^C")
-#				
-#			if data[0] == 'success':
-#				print ipAddr," responded with IP Details for ", data[12]
-#
-#				asn,space,asname = data[12].partition(' ')
-#				# Now we need to strip first extra quote and AS from ASN
-#				# and last extra quote from asname.
-#				print data
-#				print asn, asname
-#				asn = asn[3:]
-#				asname = asname[:-1]
-#				print asn, asname
-#				outputhandle.writerow({'Id':data[13],'Label':data[13],'AS#':asn,'AS Name':asname,'Country Code':data[2],'Country':data[1],'Region Code':data[3],'Region':data[4]})
-#			else:
-#				if data[1]=='"invalid query"':
-#					print data
-#					print ipAddr," is malformated, produced Invalid Query"
-#				elif data[1]=='"quota"':
-#					print "You are over quota - break your input file, or add a larger delay"
-#					print ipAddr," should be the start of your next file"
-#				else:
-#					asname = data[1]
-#					asname = asname[1:-1]
-#					print ipAddr," responded as a reserved address"
-#					outputhandle.writerow({'Id':data[2],'Label':data[2],'AS#':'','AS Name':asname,'Country Code':'XX','Country':'','Region Code':'XXX','Region':''})
-#			raw_input ("Press ENTER to continue..")		
 http.close()
 
-for key in datablock:
- print key, datablock[key]
-
-#
-## Print the CSV output headers
-#outputfile = open (outputfilename,"wb")
-#writer = csv.writer(outputfile)
-#outputhandle = csv.DictWriter(outputfile, fieldnames=outfields)
-#outputhandle.writeheader()
+# Print the CSV output headers
+outputfile = open (outputfilename,"wb")
+writer = csv.writer(outputfile)
+outputhandle = csv.DictWriter(outputfile, fieldnames=outfields)
+outputhandle.writeheader()
 
 # Now process the JSON data and output as CSV
+for ipAddr,data in datablock.iteritems():
+	outrow={"Id":ipAddr,"Label":ipAddr}
+	outrow.update(eval(data))
+	outputhandle.writerow(outrow)
+outputfile.close()
 
-#outputfile.close()
-
+# EOF
