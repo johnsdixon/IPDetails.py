ce892fe5b2b3d808fe5eda2fd8b1eb21d162a947
John S. Dixon <johnsdixon@gmail.com>
2017-10-03 18:32:28 +0100

    Update to read IP addresses into block and add details from IP-API.COM. Next step to output them all

--- IPDetails.py
+++ IPDetails.py
@@ -1,67 +1,104 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import json
+import io
 import csv
 import time
 import httplib
 
-inputfilename = "IPAddrs.txt"
-outputfilename = "IPAddrs.out"
+inputfilename = 'IPAddrs-short.txt'
+outputfilename = 'IPAddrs.out'
+csvdialect = {'__module__': 'csv', 'lineterminator': '\r\n', 'skipinitialspace': False, 'quoting': 0, '_name': 'sniffed', 'delimiter': ',', 'quotechar': '"', '__doc__': None, 'doublequote': False}
 
-# Print the CSV output headers
-outputfile = open (outputfilename,"wb")
-writer = csv.writer(outputfile)
-csvfields = ['Id','Label','AS#','AS Name','Country Code','Country','Region Code','Region']
-outputhandle = csv.DictWriter(outputfile, fieldnames=csvfields)
-outputhandle.writeheader()
+outfields = ['Id','Label','AS#','AS Name','Country Code','Country','Region Code','Region']
+apifields = ['success','Country','Country Code','Region Code','Region','City','Zip','Lat','Long','TZ','ISP Name','Org Name','ASN Name','QueryIP']
 
-# Get a connection to the webserver
-http = httplib.HTTPConnection('ip-api.com')
+datablock = {}
+
+# Get the Input File and store
 
 with open (inputfilename,"r") as inputhandle:
 	for line in inputhandle:
 		ipAddr = line.strip()
-		if ipAddr:
+		datablock.update({ipAddr:{}})
+inputhandle.close()
 
-			http.request("GET","/csv/"+ipAddr)
-			resp = http.getresponse()
+# We now have the list of IP addresses we want to look at in the datablock,
+# with an empty dict as placeholders for values
+
+#apihandle = io.StringIO()
+
+# Get a connection to the webserver
+http = httplib.HTTPConnection('ip-api.com')
+
+for ipAddr in datablock:
+		if ipAddr:
 			# Don't overload the API server,
 			# wait half a second between iterations
+			time.sleep(.5)
+
+			http.request("GET","/json/"+ipAddr)
+			resp = http.getresponse()
 
 			# This is causing issues - need to refactor for CSV 
 			# (entries with , break things
 			# e.g. "AS6939 Hurricane Electric, Inc.")
+			# data = resp.read().split(',')
 
-			data = resp.read().split(',')
-			time.sleep(.5)
-			
-			if data[0] == 'success':
-				print ipAddr," responded with IP Details for ", data[12]
+			print ipAddr, datablock[ipAddr]
 
-				asn,space,asname = data[12].partition(' ')
-				# Now we need to strip first extra quote and AS from ASN
-				# and last extra quote from asname.
-				print data
-				print asn, asname
-				asn = asn[3:]
-				asname = asname[:-1]
-				print asn, asname
-				outputhandle.writerow({'Id':data[13],'Label':data[13],'AS#':asn,'AS Name':asname,'Country Code':data[2],'Country':data[1],'Region Code':data[3],'Region':data[4]})
-			else:
-				if data[1]=='"invalid query"':
-					print data
-					print ipAddr," is malformated, produced Invalid Query"
-				elif data[1]=='"quota"':
-					print "You are over quota - break your input file, or add a larger delay"
-					print ipAddr," should be the start of your next file"
-				else:
-					asname = data[1]
-					asname = asname[1:-1]
-					print ipAddr," responded as a reserved address"
-					outputhandle.writerow({'Id':data[2],'Label':data[2],'AS#':'','AS Name':asname,'Country Code':'XX','Country':'','Region Code':'XXX','Region':''})
-			raw_input ("Press ENTER to continue..")		
+			data=resp.read()
+			print data
+			l={ipAddr:data}
+			print l
+			datablock.update(dict(l))
 
+			print ipAddr, datablock[ipAddr]
+
+#			# The new way
+#			reader = csv.reader(resp.read())
+#			print reader
+#			raw_input("Press ^C")
+#				
+#			if data[0] == 'success':
+#				print ipAddr," responded with IP Details for ", data[12]
+#
+#				asn,space,asname = data[12].partition(' ')
+#				# Now we need to strip first extra quote and AS from ASN
+#				# and last extra quote from asname.
+#				print data
+#				print asn, asname
+#				asn = asn[3:]
+#				asname = asname[:-1]
+#				print asn, asname
+#				outputhandle.writerow({'Id':data[13],'Label':data[13],'AS#':asn,'AS Name':asname,'Country Code':data[2],'Country':data[1],'Region Code':data[3],'Region':data[4]})
+#			else:
+#				if data[1]=='"invalid query"':
+#					print data
+#					print ipAddr," is malformated, produced Invalid Query"
+#				elif data[1]=='"quota"':
+#					print "You are over quota - break your input file, or add a larger delay"
+#					print ipAddr," should be the start of your next file"
+#				else:
+#					asname = data[1]
+#					asname = asname[1:-1]
+#					print ipAddr," responded as a reserved address"
+#					outputhandle.writerow({'Id':data[2],'Label':data[2],'AS#':'','AS Name':asname,'Country Code':'XX','Country':'','Region Code':'XXX','Region':''})
+#			raw_input ("Press ENTER to continue..")		
 http.close()
-inputhandle.close()
-outputfile.close()
+
+for key in datablock:
+ print key, datablock[key]
+
+#
+## Print the CSV output headers
+#outputfile = open (outputfilename,"wb")
+#writer = csv.writer(outputfile)
+#outputhandle = csv.DictWriter(outputfile, fieldnames=outfields)
+#outputhandle.writeheader()
+
+# Now process the JSON data and output as CSV
+
+#outputfile.close()
 
