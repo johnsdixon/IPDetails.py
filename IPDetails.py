01a7493bc972033c762e14b58fd5726853087f25
John S. Dixon <johnsdixon@gmail.com>
2017-12-08 14:27:29 +0000

    pylint: fixup bad indents

--- IPDetails.py
+++ IPDetails.py
@@ -60,8 +60,8 @@ def splitASdetails(combined, string):
         else:
             asn = asn[2:]
     else:
-            asn = 0
-            name = string
+        asn = 0
+        name = string
     return(asn, name)
 
 def process_address(ipAddr, http):
@@ -91,23 +91,23 @@ def process_address(ipAddr, http):
                 else:
                     wv = 'RFC2372 Loopback Network'
             elif ipa.is_private:
-                    if sys.version[0] == 3 and sys.version[1]>5:
-                        if ipa.version == 4:
-                            wv = 'RFC1918 Private Network'
-                        else:
-                            wv = 'RFC4193 Unique Local Address'
-                        u = {'Label':ipa.reverse_pointer}
+                if sys.version[0] == 3 and sys.version[1]>5:
+                    if ipa.version == 4:
+                        wv = 'RFC1918 Private Network'
                     else:
-                        if ipa.version == 4:
-                            wv = 'RFC1918 Private Network'
-                            reverse_octets = str(ipa).split('.')[::-1]
-                            reverse_pointer = '.'.join(reverse_octets) + '.in-addr.arpa'
-                            u = {'Label':reverse_pointer}
-                        else:
-                            wv = 'RFC4193 Unique Local Address'
-                            reverse_chars = ipa.exploded[::-1].replace(':', '')
-                            reverse_pointer = '.'.join(reverse_chars) + '.ip6.arpa'
-                            u = {'Label':reverse_pointer}
+                        wv = 'RFC4193 Unique Local Address'
+                    u = {'Label':ipa.reverse_pointer}
+                else:
+                    if ipa.version == 4:
+                        wv = 'RFC1918 Private Network'
+                        reverse_octets = str(ipa).split('.')[::-1]
+                        reverse_pointer = '.'.join(reverse_octets) + '.in-addr.arpa'
+                        u = {'Label':reverse_pointer}
+                    else:
+                        wv = 'RFC4193 Unique Local Address'
+                        reverse_chars = ipa.exploded[::-1].replace(':', '')
+                        reverse_pointer = '.'.join(reverse_chars) + '.ip6.arpa'
+                        u = {'Label':reverse_pointer}
             else:
                 detail = callAPI(ipAddr, http)
                 data.update(detail)
